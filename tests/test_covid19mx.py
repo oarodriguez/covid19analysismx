7ca8f8c1a7c10bdd25bb16984c6205f5edb5c8ee
Omar Abel Rodríguez-López <oarodriguez.mx@gmail.com>
2021-09-24 13:06:31 -0500

    Check the project version number

---
+++ tests/test_covid19mx.py
@@ -0,0 +1,7 @@
+"""Verify the library top-level functionality."""
+import covid19mx
+
+
+def test_version():
+    """Verify we have updated the package version."""
+    assert covid19mx.__version__ == "2021.1.0.dev0"
